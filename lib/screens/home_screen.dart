d3fe8233af241149defbb0e0723a751528768bbc
Carlos Tapia <carlostapie94@gmail.com>
2022-08-26 16:08:32 -0400

    Creación de estructura ListView del HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:weekly_challenge_2022_flutter/router/app_routes.dart';
+import 'package:weekly_challenge_2022_flutter/theme/app_theme.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final screenOptions = AppRoutes.screenOptions;
+
     return Scaffold(
       appBar: AppBar(
           title: const Center(
         child: Text('Weekly Challenge'),
       )),
-      body: const Center(
-        child: Text('HomeScreen'),
+      body: ListView.separated(
+        itemBuilder: (context, index) => ListTile(
+          title: Text(
+            screenOptions[index].name,
+          ),
+          trailing: const Icon(
+            Icons.arrow_right,
+            color: AppTheme.primary,
+          ),
+        ),
+        separatorBuilder: (_, __) => const Divider(),
+        itemCount: screenOptions.length,
       ),
     );
   }
