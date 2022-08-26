3c37645a9adfb0d614fe216a393815f934fbbed0
Carlos Tapia <carlostapie94@gmail.com>
2022-08-26 17:23:05 -0400

    Logica del fizzbuzz funcionando por consola

--- lib/screens/reto_0.dart
+++ lib/screens/reto_0.dart
@@ -5,8 +5,34 @@ class Reto0Screen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    List fizzbuzz = List.filled(101, 0);
+    int i = 1;
+    for (i; i <= 100; i++) {
+      int divThree = i % 3;
+      int divFive = i % 5;
+
+      if (divThree == 0 && divFive == 0) {
+        fizzbuzz[i] = 'fizzbuzz';
+        print(fizzbuzz[i]);
+      } else if (divThree == 0) {
+        fizzbuzz[i] = 'fizz';
+        print(fizzbuzz[i]);
+      } else if (divFive == 0) {
+        fizzbuzz[i] = 'buzz';
+        print(fizzbuzz[i]);
+      } else {
+        fizzbuzz[i] = '$i';
+        print(fizzbuzz[i]);
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text("El Famoso 'Fizz Buzz'"),
+        ),
+      ),
+      body: const Center(
         child: Text('Reto0Screen'),
       ),
     );
